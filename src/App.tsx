b330cf42b2dd7147e1f2f4e0408b37699e6f00cd
Amr Ahmed <amr.ahmed@telusinternational.com>
2023-02-24 10:33:25 +0900

    3rd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react'
 // // import Application from "./Components/Application/Application";
 // // import Counter from "./Components/Counter/Counter";
 // // import CounterTwo from "./Components/CounterTwo/CounterTwo";
 // // import MuiMode from "./Components/Mui/MuiMode";
 // // import Skills from "./Components/Skill/skill";
-import Users from "./Components/users/Users";
+import Users from './Components/users/Users'
 // import AppProvider from "./Providers/AppProvider";
 
 function App() {
@@ -16,11 +16,11 @@ function App() {
       {/* <Counter /> */}
       {/* <AppProvider>
         <MuiMode />
-      </AppProvider> */}
+      // </AppProvider> */}
       {/* <CounterTwo count={20} /> */}
       <Users />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
