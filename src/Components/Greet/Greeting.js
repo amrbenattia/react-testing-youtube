b4caa9deac17aec87c1116c0011efed9a1a55f30
Amr Ahmed <amr.ahmed@telusinternational.com>
2023-02-24 12:26:18 +0900

    removed js files

--- src/Components/Greet/Greeting.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Greeting = function (_a) {
-    var name = _a.name;
-    return <div>Hello {name ? name : 'guest'}</div>;
-};
-exports["default"] = Greeting;
